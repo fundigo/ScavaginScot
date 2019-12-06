31523c7d7c43b2b75e0127a293cbecd82ba76f7a
dylandeceulaer <dylan.deceulaer@student.howest.be>
2019-12-06 11:07:55 +0100

    FirstLevel

--- Scavaging Scot/Assets/Controller.cs
+++ Scavaging Scot/Assets/Controller.cs
@@ -7,25 +7,32 @@ using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
+    public PlayerLogic controller;
+    public GameObject Player;
+    private Animator PlayerAnimator;
     public GameObject ActiveImage;
     public Sprite[] ControllerActives = new Sprite[4];
-    public Button ControllerUp;
-    public Button ControllerRight;
-    public Button ControllerDown;
-    public Button ControllerLeft;
+    public float WalkingSpeed = 0.2f;
+    private bool isMovingLeft = false;
+    private bool isMovingRight = false;
+    private bool isJumping = false;
+    public bool AreControllsEnabled = true;
     void Start()
     {
-
-
+        PlayerAnimator = Player.transform.GetChild(0).GetComponent<Animator>();
     }
 
 
-
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(isMovingLeft) controller.Move(-(WalkingSpeed*Time.fixedDeltaTime), false, false);
+        if (isMovingRight) controller.Move(WalkingSpeed*Time.fixedDeltaTime, false, false);
+        if (isJumping)
+        {
+            controller.Move(0, false, isJumping);
+            isJumping = false;
+        }
     }
     public void doAction(string action)
     {
@@ -34,15 +41,55 @@ public class Controller : MonoBehaviour
         {
             case "up":
                 ActiveImage.GetComponent<Image>().sprite = ControllerActives[0];
+                if (AreControllsEnabled) { 
+                    PlayerAnimator.SetBool("IsJumping", true);
+                    isJumping = true;
+                }
                 break;
             case "right":
                 ActiveImage.GetComponent<Image>().sprite = ControllerActives[1];
+                if (AreControllsEnabled)
+                {
+                    PlayerAnimator.SetBool("Right", true);
+                    PlayerAnimator.SetBool("IsMoving", true);
+                    isMovingRight = true;
+                    PlayerAnimator.SetBool("Left", false);
+                }
                 break;
             case "down":
                 ActiveImage.GetComponent<Image>().sprite = ControllerActives[2];
                 break;
             case "left":
                 ActiveImage.GetComponent<Image>().sprite = ControllerActives[3];
+                if (AreControllsEnabled)
+                {
+                    PlayerAnimator.SetBool("Left", true);
+                    PlayerAnimator.SetBool("IsMoving", true);
+                    isMovingLeft = true;
+                    PlayerAnimator.SetBool("Right", false);
+                } 
+                break;
+            case "leftUp":
+                ActiveImage.GetComponent<Image>().sprite = ControllerActives[3];
+                if (AreControllsEnabled)
+                {
+                    PlayerAnimator.SetBool("Right", false);
+                    PlayerAnimator.SetBool("IsJumping", true);
+                    PlayerAnimator.SetBool("Left", true);
+                    isJumping = true;
+                    isMovingLeft = true;
+                }
+                break;
+            case "rightUp":
+                ActiveImage.GetComponent<Image>().sprite = ControllerActives[3];
+                if (AreControllsEnabled)
+                {
+                    PlayerAnimator.SetBool("Left", false);
+                    PlayerAnimator.SetBool("IsJumping", true);
+                    PlayerAnimator.SetBool("Right", true);
+                    isJumping = true;
+                    isMovingRight = true;
+                }
                 break;
             default:
                 break;
@@ -50,6 +97,13 @@ public class Controller : MonoBehaviour
     }
     public void cancelAction ()
     {
-        ActiveImage.SetActive(false);
+        if (AreControllsEnabled)
+        {
+            isMovingLeft = false;
+            isMovingRight = false;
+            ActiveImage.SetActive(false);
+            PlayerAnimator.SetBool("IsMoving", false);
+            PlayerAnimator.SetBool("IsJumping", false);
+        }
     }
 }
---
+++ Scavaging Scot/Assets/controllWheel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controllWheel : MonoBehaviour
+{
+
+    public GameObject[] chains = new GameObject[2];
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        foreach (GameObject chain in chains)
+        {
+            chain.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, -(Time.time * 0.5f));
+        }
+    }
+
+}
